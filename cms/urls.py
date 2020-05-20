8ccf1c248bc796f4a45677ef0ccd7768c849b9e6
ktan424 <kokitanaka424@gmail.com>
2020-05-21 03:14:59 +0900

    top

--- cms/urls.py
+++ cms/urls.py
@@ -6,6 +6,7 @@ app_name = 'cms'
 
 urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
+    path('tutorial/', views.Tutorial.as_view(), name='tutorial'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.UserCreate.as_view(), name='signup'),
